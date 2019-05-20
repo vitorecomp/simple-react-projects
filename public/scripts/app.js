1b92348d8e7e00a76de616f41b5053d19e577a7d
vitor <vitor.ecomp@gmail.com>
2019-05-20 10:41:12 -0300

    Git fazendo o fluxo e es6

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,45 @@
 'use strict';
 
-console.log('teste');
-
 //functions
 var app = {
 	title: 'Indesion App',
 	subtitle: 'This is a test',
-	location: 'newYork'
+	options: ['one', 'two']
+};
 
-	//html template
-};var template = React.createElement(
+var optionsRender = function optionsRender(app) {
+	if (app.options.length == 0) return React.createElement(
+		'p',
+		null,
+		'No Opitons\' '
+	);
+
+	return React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'p',
+			null,
+			'Here are your options'
+		),
+		React.createElement(
+			'ol',
+			null,
+			React.createElement(
+				'li',
+				null,
+				app.options[0]
+			),
+			React.createElement(
+				'li',
+				null,
+				app.options[1]
+			)
+		)
+	);
+};
+//html template
+var template = React.createElement(
 	'div',
 	null,
 	React.createElement(
@@ -17,13 +47,14 @@ var app = {
 		null,
 		app.title
 	),
-	React.createElement(
+	app.subtitle ? React.createElement(
 		'p',
 		null,
 		' ',
 		app.subtitle,
 		' '
-	)
+	) : undefined,
+	optionsRender(app)
 );
 
 //render
--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
-console.log('teste')
-
 //functions
-let app = {
+const app = {
 	title: 'Indesion App',
 	subtitle: 'This is a test',
-	location: 'newYork'
+	options: [
+		'one',
+		'two'
+	]
 }
 
+const optionsRender = (app) => {
+	if (app.options.length == 0)
+		return <p>No Opitons' </p>
 
+	return <div>
+		<p>Here are your options</p>
+		<ol>
+			<li>{app.options[0]}</li>
+			<li>{app.options[1]}</li>
+		</ol>
+	</div>
+}
 //html template
-var template = (
+const template = (
 	<div>
 		<h1>{app.title}</h1>
-		<p> {app.subtitle} </p>
+		{app.subtitle ? <p> {app.subtitle} </p> : undefined}
+		{optionsRender(app)}
 	</div>
 )
+
 
 
 //render
